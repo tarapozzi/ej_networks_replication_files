9f32024da1f8fb3611a54b5c16519b6297f9acfe
tarapozzi <tarapozzi@u.boisestate.edu>
2024-11-25 12:58:21 -0800

    organized script & added robustness checks

--- scripts/1_functions.R
+++ scripts/1_functions.R
@@ -90,9 +90,7 @@ gather_coefs_numeric <-  function(m, name) {
   return(d)
 } 
 
-# Contrasts function
-# Nesting: if factor levels of one variable are only meaningful within the context of another factor, that could require specifying nesting.
-# Loop through the parameters
+# contrasts function
 contrasts_calc <- function(m, spec) {
   d.50 <- emmeans(m, specs = as.formula(paste0("~", spec)), nesting = NULL) %>%
     contrast(method = "eff", adjust = "bonferroni") %>%
@@ -113,7 +111,82 @@ contrasts_calc <- function(m, spec) {
   return(d)
 }
 
-# gather coefficient estimates from model results 
+contrasts_calc_ejfactor <- function(spec) {
+  d.50 <- emmeans(m_full_ejfactor, specs = as.formula(paste0("~", spec)), nesting = NULL) %>%
+    contrast(method = "eff", adjust = "bonferroni") %>%
+    confint(level = 0.50) %>%
+    mutate(variable = spec,
+           .width = .50) %>%
+    rename(.lower = lower.HPD, .upper = upper.HPD)
+  
+  d.89 <- emmeans(m_full_ejfactor, specs = as.formula(paste0("~", spec)), nesting = NULL) %>%
+    contrast(method = "eff", adjust = "bonferroni") %>%
+    confint(level = 0.89) %>%
+    mutate(variable = spec,
+           .width = .89) %>%
+    rename(.lower = lower.HPD, .upper = upper.HPD)
+  
+  d <- rbind(d.50, d.89)
+  
+  return(d)
+}
+
+# Categorical coefficient estimates 
+gather_coefs_categorical <-  function(m, name){
+# use emmeans to calculate intervals:
+# The basic sequence is that you create the comparison with emmeans, then calculate contrasts (with method='eff' to calculate the difference from average and adjust='bonferroni' for the Bonferroni method of correcting for multiple testing). Then since you want the confidence interval instead of the estimates, you pass the result to the confit function. 
+# columns
+colnames(m$data)
+
+# predictor list
+cat_predictors  <- c("np_match", "geo_diff_cat", "c_diff_cat", "ej_diff_cat")
+
+# empty data.frame for for loop calc
+contrasts <- data.frame()
+
+# iterate on factors from the paper model
+for(i in cat_predictors) {
+  d <- contrasts_calc(m = m, spec = i)
+  contrasts <- rbind(contrasts, d)
+}
+
+# clean up df so it is ready to plot
+coefs_cat <- contrasts %>%
+  mutate(contrast = case_when(
+    contrast == "no_np_homophily effect" ~ "Both Non-501c3",
+    contrast == "lower effect" & variable == "np_match" ~ "Non-501c3 Seeking 501c3",
+    contrast == "higher effect" & variable == "np_match" ~ "501c3 Seeking Non-501c3",
+    contrast == "np_homophily effect" ~ "Both 501c3",
+    contrast == "local_match effect" ~ "Both Local",
+    contrast == "smaller effect" ~ "Regional Seeking Local",
+    contrast == "bigger effect" ~ "Local Seeking Regional",
+    contrast == "regional_match effect"  ~ "Both Regional",
+    contrast == "match effect" & variable == "c_diff_cat" ~ "Same Capacity",
+    contrast == "lower effect" & variable == "c_diff_cat" ~ "Lower Capacity Seeking Higher",
+    contrast == "higher effect" & variable == "c_diff_cat" ~ "Higher Capacity Seeking Lower",
+    contrast == "match effect" & variable == "ej_diff_cat" ~ "Matching EJ Commitment",
+    contrast == "lower effect" & variable == "ej_diff_cat" ~ "More EJ Seeking Less EJ", 
+    contrast == "higher effect" & variable == "ej_diff_cat" ~ "Less EJ Seeking More EJ"
+  )) %>%
+  mutate(mode = case_when(
+    variable %in% c("geo_diff_cat","ej_diff_cat") ~ "Boundary Definition",
+    variable %in% c("np_match", "c_diff_cat") ~ "Resource Exchange"
+  )) %>%
+  mutate(variable = case_when(
+    variable == "geo_diff_cat" ~ "Geography",
+    variable == "ej_diff_cat" ~ "EJ Commitment", 
+    variable == "np_match" ~ "501c3 Status", 
+    variable == "c_diff_cat" ~ "Capacity"
+  )) %>%
+  mutate(hyp_type = "Relational") %>%
+  mutate(mode = factor(mode, c("Resource Exchange", "Boundary Definition")),
+         variable_type = "Relational") %>%
+  rename(par = contrast,
+         value = estimate) # make it match the numerical dataframe
+}
+
+
+# Coefficient estimates from model results without doing categorical contrasts
 gather_coefs <-  function(m, name) {
   variables <- c("b_ego_capacity_n", "b_alter_capacity_n", "b_factorc_diff_catlower", "b_factorc_diff_cathigher", "b_count_ego_collaboratives", "b_count_alter_collaboratives", "b_overlap_collab", "b_factornp_matchlower", "b_factornp_matchhigher", "b_factornp_matchno_np_homophily", "b_factorej_diff_catlower", "b_factorej_diff_cathigher", "b_count_ego_issues", "b_count_alter_issues",  "b_i_match", "b_factorgeo_diff_catregional_match", "b_factorgeo_diff_catsmaller", "b_factorgeo_diff_catbigger", "b_distance_n")  
   d <- m %>% 
@@ -271,154 +344,6 @@ gather_coefs <-  function(m, name) {
   return(d)
 } 
 
-# gather coefficient estimates from model results 
-gather_coefs_re <-  function(m, name) {
-  variables <- c("b_ego_capacity_n","b_ego_np_501c3", "b_alter_np_501c3", "b_alter_capacity_n", "b_c_diff_n", "b_np_match", "b_c_diff_catlower","b_c_diff_cathigher", "b_count_alter_collaboratives", "b_count_ego_collaboratives", "b_overlap_collab", "b_np_matchnp_homophily", "b_np_matchlower", "b_np_matchhigher")
-  d <- m %>% 
-    tidy_draws() %>%
-    gather_variables() %>%
-    filter(.variable %in% variables) %>% 
-    dplyr:: rename(par=.variable, value=.value) %>%
-    median_qi(.width = c(.89, .5))%>%
-    mutate(type = case_when(
-      par == "b_ego_capacity_n" ~ "Capacity",
-      par == "b_alter_capacity_n" ~ "Capacity",
-      par == "b_alter_np_501c3" ~ "501c3 Status", 
-      par == "b_c_diff_catlower" ~ "Capacity",
-      par == "b_c_diff_cathigher" ~ "Capacity",
-      par == "b_count_alter_collaboratives_s" ~ "Collaborative \nMembership",
-      par == "b_overlap_collab" ~ "Collaborative \nMembership",
-      par == "b_ego_np_501c3" ~ "501c3 Status", 
-      par == "b_count_ego_collaboratives" ~ "Collaborative \nMembership", 
-      par == "b_count_alter_collaboratives" ~ "Collaborative \nMembership", 
-      par == "b_np_matchnp_homophily" ~ "501c3 Status",
-      par == "b_np_matchlower" ~ "501c3 Status", 
-      par == "b_np_matchhigher" ~ "501c3 Status"
-    )) %>%
-    mutate(hyp = case_when(
-      par == "b_alter_np_501c3" ~ "Individual", 
-      par == "b_alter_capacity_n" ~ "Individual",
-      par == "b_ego_capacity_n" ~ "Individual",
-      par == "b_np_match" ~ "Individual",
-      par == "b_c_diff_catlower" ~ "Relational", 
-      par == "b_c_diff_cathigher" ~ "Relational", 
-      par == "b_count_alter_collaboratives_s" ~ "Individual",
-      par == "b_ego_np_501c3" ~ "Individual", 
-      par == "b_c_diff_n" ~ "Relational", 
-      par == "b_count_ego_collaboratives" ~ "Individual", 
-      par == "b_count_alter_collaboratives" ~ "Individual", 
-      par == "b_overlap_collab" ~ "Relational", 
-      par == "b_np_matchnp_homophily" ~ "Relational",
-      par == "b_np_matchlower" ~ "Relational",
-      par == "b_np_matchhigher" ~ "Relational"
-    )) %>%
-    mutate(type = factor(type, c("Capacity", "Boundary Definition"))) %>%
-    mutate(hyp = factor(hyp, c("Relational", "Individual"))) %>%
-    mutate(par = case_when(
-      par == "b_alter_np_501c3" ~ "Alter 501c3 Status", 
-      par == "b_alter_capacity_n" ~ "Alter Capacity", 
-      par == "b_ego_capacity_n" ~ "Ego Capacity",
-      par == "b_np_match" ~ "Homophily: 501c3 \nStatus Match",
-      par == "b_c_diff_catlower" ~ "Heterophily: Lower Capacity \nAlter than Ego",
-      par == "b_c_diff_cathigher" ~ "Heterophily: Higher Capacity \nAlter than Ego",
-      par == "b_count_alter_collaboratives_s" ~ "Alter No. of Collaboratives",
-      par == "b_overlap_collab" ~ "Homophily: Collaborative Membership \nOverlap", 
-      par == "b_ego_np_501c3" ~ "Ego 501c3 Status", 
-      par == "b_count_ego_collaboratives" ~ "Ego No. of Collaboratives", 
-      par == "b_count_alter_collaboratives" ~ "Alter No. of Collaboratives", 
-      par == "b_np_matchnp_homophily" ~ "Homophily: Both Groups without \n501c3",
-      par == "b_np_matchlower" ~ "Heterophily: 501c3 Ego Seeking \na Non-501c3 Alter", 
-      par == "b_np_matchhigher" ~ "Heterophily: Non-501c3 Ego Seeking \na 501c3 Alter"
-    )) %>%
-    mutate(variable = case_when(
-      str_detect(par, regex("Capacity*")) ~ "Resource Capacity",
-      str_detect(par, regex("Collaborative*")) ~ "Collaborative",
-      str_detect(par, regex("501c3*")) ~ "501c3 Status"
-    )) %>%
-    mutate(ordering = -as.integer(factor(hyp)) + value) %>%
-    mutate(par = fct_reorder(par, ordering,  .desc = F)) %>%
-    mutate(model = name)
-  return(d)
-} 
-
-# gather coefficient estimates from model results 
-gather_coefs_bd <-  function(m, name) {
-  variables <- c("b_ego_ej_mission", "b_alter_ej_mission", "b_alter_localnonlocal", "b_distance_n","b_i_match_s", "b_ej_diff_catlower", "b_ej_diff_cathigher", "b_geo_diff_catbigger", "b_geo_diff_catsmaller", "b_ego_localregional","b_alter_localregional",  "b_geo_diff_catlocal_match", "b_geo_diff_catregional_match", "b_count_ego_issues", "b_count_alter_issues", "b_i_match")
-  d <- m %>% 
-    tidy_draws() %>%
-    gather_variables() %>%
-    filter(.variable %in% variables) %>% 
-    dplyr:: rename(par=.variable, value=.value) %>%
-    median_qi(.width = c(.89, .5))%>%
-    mutate(type = case_when(
-      par == "b_alter_ej_mission" ~ "Boundary Definition", 
-      par == "b_ego_ej_mission" ~ "Boundary Definition", 
-      par == "b_distance_n" ~ "Boundary Definition", 
-      par == "b_alter_localnonlocal" ~ "Boundary Definition",
-      par == "b_i_match_s" ~ "Boundary Definition", 
-      par == "b_ej_diff_catlower" ~ "Boundary Definition",
-      par == "b_ej_diff_cathigher" ~ "Boundary Definition",
-      par == "b_alter_localnonlocal" ~ "Boundary Definition",
-      par == "b_count_ego_issues" ~ "Boundary Definition", 
-      par == "b_count_alter_issues" ~ "Boundary Definition",
-      par == "b_i_match" ~ "Boundary Definition", 
-      par == "b_geo_diff_catbigger" ~ "Boundary Definition", 
-      par == "b_geo_diff_catsmaller" ~ "Boundary Definition", 
-      par == "b_ego_localregional" ~ "Boundary Definition",
-      par == "b_alter_localregional" ~ "Boundary Definition",
-      par == "b_geo_diff_catlocal_match" ~ "Boundary Definition", 
-      par == "b_geo_diff_catregional_match" ~ "Boundary Definition"
-    )) %>%
-    mutate(hyp = case_when(
-      par == "b_ego_ej_mission" ~ "Individual",
-      par == "b_count_alter_issues_s" ~ "Individual", 
-      par == "b_alter_ej_mission" ~ "Individual", 
-      par == "b_distance_n" ~ "Relational", 
-      par == "b_alter_localnonlocal" ~ "Relational",  
-      par == "b_ej_diff_catlower" ~ "Relational",
-      par == "b_ej_diff_cathigher" ~ "Relational",
-      par == "b_geo_diff_catregional_match" ~ "Relational",
-      par == "b_geo_diff_catlocal_match" ~ "Relational",
-      par == "b_count_ego_issues" ~ "Individual", 
-      par == "b_count_alter_issues" ~ "Individual",
-      par == "b_i_match" ~ "Relational", 
-      par == "b_geo_diff_catbigger" ~ "Relational", 
-      par == "b_geo_diff_catsmaller" ~ "Relational", 
-      par == "b_ego_localregional" ~ "Individual", 
-      par == "b_alter_localregional" ~ "Individual"
-    )) %>%
-    mutate(type = factor(type, c("Capacity", "Boundary Definition"))) %>%
-    mutate(hyp = factor(hyp, c("Relational", "Individual"))) %>%
-    mutate(par = case_when(
-      par == "b_ego_ej_mission" ~ "Ego EJ Commitment",
-      par == "b_count_alter_issues_s" ~ "Alter No. of Issues", 
-      par == "b_alter_ej_mission" ~ "Alter EJ Commitment", 
-      par == "b_distance_n" ~ "Heterophily: Distance Between \nHome Offices", 
-      par == "b_alter_localnonlocal" ~  "Alter Regional Group", 
-      par == "b_i_match_s" ~ "Homophily: Issue Overlap", 
-      par == "b_ej_diff_catlower" ~ "Heterophily: Lower Alter EJ \nCommitment than Ego",
-      par == "b_ej_diff_cathigher" ~ "Heterophily: Higher Alter EJ \nCommitment than Ego",
-      par == "b_geo_diff_catlocal_match" ~ "Homophilly: Both Local Groups",
-      par == "b_geo_diff_catregional_match" ~ "Homophily: Both Regional Groups",
-      par == "b_count_ego_issues" ~ "Ego No. of Issues", 
-      par == "b_count_alter_issues" ~ "Alter No. of Issues",
-      par == "b_i_match" ~ "Homophily: No. of Matching Issues", 
-      par == "b_geo_diff_catbigger" ~ "Heterophily: Local Group seeking \nRegional Group", 
-      par == "b_geo_diff_catsmaller" ~ "Heterophily: Regional Group seeking \nLocal Group", 
-      par == "b_ego_localregional" ~ "Ego Regional Group", 
-      par == "b_alter_localregional" ~ "Alter Regional Group"
-    )) %>%
-    mutate(variable = case_when(
-      str_detect(par, regex("Commitment*")) ~ "EJ Commitment", 
-      str_detect(par, regex("Issues*")) ~ "EJ Issues", 
-      str_detect(par, regex("Group*")) ~ "Geography", 
-      str_detect(par, regex("Distance*")) ~ "Geography"
-    )) %>%
-    mutate(ordering = -as.integer(factor(hyp)) + value) %>%
-    mutate(par = fct_reorder(par, ordering,  .desc = F)) %>%
-    mutate(model = name)
-  return(d)
-} 
 # Combine two models into one coefficient plot
 combine_coefs_plot2 <- function(m1, m2){
   df <- bind_rows(m1, m2)
